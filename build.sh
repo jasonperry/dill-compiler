da31b843781b2ee4e5156661cc04dcdcddc3bc9e
Jason Perry <sh.jperry@gmail.com>
2021-05-19 16:46:06 -0500

    build script improvements and gitignore for .exes

--- build.sh
+++ build.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-exename=a.out
 basedir=~/code/datlang
 incdir=$basedir/stdlib
-outdir=$basedir/out    # if fresh clone, don't forget to ~mkdir out~
+# outdir=$basedir/out    # if fresh clone, don't forget to ~mkdir out~
+
+if [ $# -lt 1 ]; then
+    echo "Specify source files (last becomes executable)"
+    exit
+fi
 
 for sf in $@; do
     if dillc.exe -I $incdir "$sf"; then
 	name=$(basename "$sf" .dl)
-	mv -f "$name.o" $outdir/
-	objfiles="$objfiles $outdir/$name.o"
+	# mv -f "$name.o" $outdir/
+	objfiles="$objfiles $name.o"
     else
 	echo "Compile failed."
 	exit
     fi
+    exename=$name.exe
 done
 
-# last one on command line is automatically name?
-if clang -o out/$exename $objfiles stdlib/stdio.ll
+if clang -o $exename $objfiles $incdir/stdio.ll
 then
-    echo "Successfully built executable out/$exename"
+    echo "Successfully built $exename"
 fi
